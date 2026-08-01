23cf9853f460208c77d70c7521a4c058082e6a0b
devjh98 <devjoonhee@gmail.com>
2026-08-01 19:40:27 +0900

    Modify run_onnx and .gitignore

--- run_onnx.py
+++ run_onnx.py
@@ -2,15 +2,38 @@ import psutil
 import os
 import onnxruntime as ort
 import numpy as np
+import time
 
 process=psutil.Process(os.getpid())
 process.nice(psutil.HIGH_PRIORITY_CLASS)
+process.cpu_affinity([0])
 
-session=ort.InferenceSession("mid_balanced.onnx")
+so=ort.SessionOptions()
+so.intra_op_num_threads=1
+so.inter_op_num_threads=1
 
+session=ort.InferenceSession(
+    "model_onnx/mid_balanced.onnx",
+    sess_options=so,
+    providers=["CPUExecutionProvider"]
+    )
+
+WARMUP=200
+N=1000
 x=np.random.rand(1,3,32,32).astype(np.float32)
 
-for i in range(100000):
+for _ in range(WARMUP):
     y=session.run(None, {"input":x})
 
+start=time.perf_counter()
+
+for i in range(N):
+    y=session.run(None, {"input":x})
+
+end=time.perf_counter()
+
+elapsed=end-start
+avg_latency = elapsed/1000
+
 print(y[0].shape)
+print(f"avg_latency: {avg_latency*1000:3f}ms")
